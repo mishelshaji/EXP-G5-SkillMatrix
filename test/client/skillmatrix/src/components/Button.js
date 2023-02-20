4bc650ab7cc69cf12d8153e7d2dcc63a3ba9f741
jometjoseph <jometjoseph103@gmail.com>
2023-02-20 11:13:01 +0530

    Restored client files

--- test/client/skillmatrix/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import '../components/Button.css';
-
-function Button(props) {
-    return (
-        <button id={props.id} >{props.value}</button>
-    )
-}
-export default Button;
